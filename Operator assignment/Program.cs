3b817c0ea07f63bd5ee2c6aa38880874ad778722
Xavier Barnes <xdbcoder44@gmail.com>
2022-08-08 00:12:47 -0400

    Add project files.

---
+++ Operator assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Selectionstatements
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+
+            int mod = a % b;
+
+            if (a == 17 && b == 4) ;
+
+            Console.WriteLine($"{a}/{b} is {b} remainder {mod} ");
+        }
+
+
+
+
+    }
+
+}
